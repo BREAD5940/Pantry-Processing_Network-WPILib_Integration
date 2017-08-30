5b5097ac158fb1df19b24d0a5ea2684a6728ee3d
BertTurtle <mbentley124@gmail.com>
2017-08-29 18:01:36 -0700

    Added documentation
    In DoubleSolenode.

--- src/org/team5940/pantry/processing_network/wpilib/output/pneumatics/DoubleSolenoidNode.java
+++ src/org/team5940/pantry/processing_network/wpilib/output/pneumatics/DoubleSolenoidNode.java
@@ -11,15 +11,36 @@ import org.team5940.pantry.processing_network.ValueNode;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 /**
+ * This is used to set the values of an array of DoubleSolenoids based on the
+ * value from a ValueNode. All of the DoubleSolenoids will have their value set
+ * at the same time.
+ * 
  * @author Michael Bentley
- *
  */ // TODO rename this to DoubleSolenode.
 public class DoubleSolenoidNode extends Node {
 
 	ValueNode<? extends DoubleSolenoid.Value> solenoidValueNode;
 	DoubleSolenoid[] doubleSolenoids;
 
-	public DoubleSolenoidNode(Network network, boolean requireUpdate, ValueNode<? extends DoubleSolenoid.Value> solenoidValueNode, DoubleSolenoid... doubleSolenoids)
+	/**
+	 * Creates a new DoubleSolenoidNode. This sets the value of multiple
+	 * doubleSolenoids based on the desired value.
+	 * 
+	 * @param network
+	 *            The Network.
+	 * @param requireUpdate
+	 *            If it requires and update every cycle.
+	 * @param solenoidValueNode
+	 *            The value to set DoubleSolenoids to.
+	 * @param doubleSolenoids
+	 *            DoubleSolenoids to set value of.
+	 * @throws IllegalArgumentException
+	 *             If argument is null.
+	 * @throws IllegalStateException
+	 *             If network is started.
+	 */
+	public DoubleSolenoidNode(Network network, boolean requireUpdate,
+			ValueNode<? extends DoubleSolenoid.Value> solenoidValueNode, DoubleSolenoid... doubleSolenoids)
 			throws IllegalArgumentException, IllegalStateException {
 		super(network, requireUpdate, solenoidValueNode);
 
