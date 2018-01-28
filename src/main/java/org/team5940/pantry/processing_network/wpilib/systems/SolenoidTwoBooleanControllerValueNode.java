0ee47a798171b6642837c6abb383894754f08fad
BertTurtle <mbentley124@gmail.com>
2018-01-27 16:12:01 -0800

    Removed buttons specification in solenoid shifting

--- src/main/java/org/team5940/pantry/processing_network/wpilib/systems/SolenoidTwoBooleanControllerValueNode.java
+++ src/main/java/org/team5940/pantry/processing_network/wpilib/systems/SolenoidTwoBooleanControllerValueNode.java
@@ -3,77 +3,80 @@ package org.team5940.pantry.processing_network.wpilib.systems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
+import org.team5940.pantry.logging.LoggingUtils;
 import org.team5940.pantry.logging.loggers.Logger;
 import org.team5940.pantry.processing_network.Network;
 import org.team5940.pantry.processing_network.ValueNode;
-import org.team5940.pantry.processing_network.ProcessingNetworkUtils;
 
 /**
- * This returns a value for a Solenoid to use. This uses two separate buttons to
- * switch back and forth. Each is designated a solenoid {@linkplain Value} and
- * then when that button is activated the solenoids are set to that value.
+ * This returns a value for a Solenoid to use. This uses two separate booleans
+ * to switch back and forth. Each is designated a solenoid {@linkplain Value}
+ * and then when that boolean is true the solenoids are set to that value.
  * 
  * @author Michael Bentley
  *
  */
-public class SolenoidTwoButtonControllerValueNode extends ValueNode<DoubleSolenoid.Value> {
+public class SolenoidTwoBooleanControllerValueNode extends ValueNode<DoubleSolenoid.Value> {
 
 	/**
 	 * The ValueNode that sets the value of the solenoids to kReverse when
 	 * activated. When both ValueNodes are pressed this takes precedent.
 	 */
-	ValueNode<? extends Boolean> shiftReverseButton;
+	ValueNode<? extends Boolean> shiftReverseBoolean;
 
 	/**
 	 * The ValueNode that sets the value of the solenoids to kForward when
 	 * activated.
 	 */
-	ValueNode<? extends Boolean> shiftForwardButton;
+	ValueNode<? extends Boolean> shiftForwardBoolean;
 
 	/**
 	 * The last set position of the Solenoid. Used so the value of the solenoid is
-	 * unchanged when neither button is pressed.
+	 * unchanged when neither boolean is true.
 	 */
 	DoubleSolenoid.Value currentPosition;
 
 	/**
-	 * This is used to shift up and down with two separate buttons. The shift up and
-	 * down buttons do not have to be buttons, but it is recommended for them to be
-	 * buttons. They could be any value node that returns a boolean. If both buttons
-	 * are pressed at once then {@linkplain Value#kReverse} takes priority.
+	 * This is used to shift up and down with two separate boolean ValueNodes. The
+	 * shift up and down booleans are typically from buttons but not always. They
+	 * could be any value node that returns a boolean. If both booleans are true at
+	 * once then {@linkplain Value#kReverse} takes priority.
 	 * 
 	 * @param network
 	 *            This' Network
 	 * @param logger
 	 *            This' Logger
-	 * @param shiftReverseButton
+	 * @param label
+	 *            This' label.
+	 * @param shiftReverseBoolean
 	 *            When true it sets the piston's state to
 	 *            {@linkplain Value#kReverse}
-	 * @param shiftForwardButton
+	 * 
+	 * @param shiftForwardBoolean
 	 *            When true it sets the piston's state to
 	 *            {@linkplain Value#kForward}
 	 * @param startingPosition
 	 *            The starting position of the piston.
 	 */
-	public SolenoidTwoButtonControllerValueNode(Network network, Logger logger,
-			ValueNode<? extends Boolean> shiftReverseButton, ValueNode<? extends Boolean> shiftForwardButton,
+	public SolenoidTwoBooleanControllerValueNode(Network network, Logger logger, String label,
+			ValueNode<? extends Boolean> shiftReverseBoolean, ValueNode<? extends Boolean> shiftForwardBoolean,
 			DoubleSolenoid.Value startingPosition) throws IllegalArgumentException, IllegalStateException {
-		super(network, logger, shiftReverseButton, shiftForwardButton);
+		super(network, logger, label, shiftReverseBoolean, shiftForwardBoolean);
 
-		ProcessingNetworkUtils.checkArgument(startingPosition);
+		LoggingUtils.checkArgument(startingPosition);
 
-		this.shiftReverseButton = shiftReverseButton;
-		this.shiftForwardButton = shiftForwardButton;
+		this.shiftReverseBoolean = shiftReverseBoolean;
+		this.shiftForwardBoolean = shiftForwardBoolean;
 		this.currentPosition = startingPosition;
 	}
 
 	@Override
 	protected Value updateValue() {
-		if (shiftReverseButton.getValue()) {
+		if (shiftReverseBoolean.getValue()) {
 			this.currentPosition = DoubleSolenoid.Value.kReverse;
 			return DoubleSolenoid.Value.kReverse;
 		}
-		if (shiftForwardButton.getValue()) {
+		if (shiftForwardBoolean.getValue()) {
 			this.currentPosition = DoubleSolenoid.Value.kForward;
 			return DoubleSolenoid.Value.kForward;
 		}
