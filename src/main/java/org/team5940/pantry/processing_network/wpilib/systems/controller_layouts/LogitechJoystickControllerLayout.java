a08359b0d8e070e9281ac33e1d654f65c03b3a95
BertTurtle <mbentley124@gmail.com>
2018-02-17 17:18:16 -0800

    Added Logitech joystick layout

---
+++ src/main/java/org/team5940/pantry/processing_network/wpilib/systems/controller_layouts/LogitechJoystickControllerLayout.java
@@ -0,0 +1,41 @@
+package org.team5940.pantry.processing_network.wpilib.systems.controller_layouts;
+
+public interface LogitechJoystickControllerLayout {
+
+	public interface Axis {
+		public static final int JOYSTICK_X = 0;
+
+		public static final int JOYSTICK_Y = 1;
+
+		public static final int JOYSTICK_ROTATE = 2;
+
+		public static final int SLIDER = 3;
+	}
+
+	public interface Buttons {
+		public static final int TRIGGER = 1;
+
+		public static final int BUTTON_2 = 2;
+
+		public static final int BUTTON_3 = 3;
+
+		public static final int BUTTON_4 = 4;
+
+		public static final int BUTTON_5 = 5;
+
+		public static final int BUTTON_6 = 6;
+
+		public static final int BUTTON_7 = 7;
+
+		public static final int BUTTON_8 = 8;
+
+		public static final int BUTTON_9 = 9;
+
+		public static final int BUTTON_10 = 10;
+
+		public static final int BUTTON_11 = 11;
+
+		public static final int BUTTON_12 = 12;
+	}
+
+}
